17d69f88cbe778bce012f9361993fc30f6dcd5e6
Nagy Roland <roliboy@protonmail.com>
2021-09-08 22:41:35 +0300

    add header to project list

--- src/routes/projects/index.svelte
+++ src/routes/projects/index.svelte
@@ -4,7 +4,6 @@
 
 <script lang="ts">
 	import Footer from '$lib/Footer.svelte';
-	import Project from '$lib/project/Project.svelte';
 	import projects from '../../data/projects.json';
 </script>
 
@@ -12,11 +11,27 @@
 	<title>Projects</title>
 </svelte:head>
 
+<header class="w-full half bg-header bg-center bg-no-repeat bg-cover">
+	<!-- <nav>
+		<div class="flex justify-end p-16 text-xl">
+			<a href="#projects" class="link-underline ml-16">Projects</a>
+			<a href="#about" class="link-underline ml-16">About</a>
+			<a href="#contact" class="link-underline ml-16">Contact</a>
+		</div>
+	</nav> -->
+	<div class="flex h-full items-center">
+		<div class="p-16">
+			<h1 class="text-6xl text-primary mb-4">Projects</h1>
+		</div>
+	</div>
+</header>
+
 <div class="container px-8 mx-auto">
 	<div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-8">
 		{#each projects as project}
 			<a
-				href="#acolo"
+				sveltekit:prefetch
+				href={project.slug}
 				class="shadow rounded overflow-hidden cursor-pointer bg-background-light transition-all hover:shadow-2xl hover:scale-105"
 			>
 				<img
@@ -42,3 +57,12 @@
 	</div>
 </div>
 <Footer />
+
+<style lang="postcss">
+	.half {
+		height: 50vh;
+	}
+	.heading {
+		@apply text-6xl text-primary after:block after:h-0.5 after:w-32 after:bg-secondary after:my-6;
+	}
+</style>
