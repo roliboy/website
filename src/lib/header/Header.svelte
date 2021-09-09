382d5e8a3bdd1e9a9086555f05beec652e2810dc
Nagy Roland <roliboy@protonmail.com>
2021-09-09 11:37:35 +0300

    add back button to header

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
-	export let height: string;
+	import { goto } from '$app/navigation';
+
+	export let height: string = 'h-screen';
+	export let showBack: boolean = false;
+
+	const goBack = () => {
+		goto(document.referrer || '/');
+	};
 </script>
 
 <div class="w-full {height}">
@@ -10,6 +17,14 @@
 				<slot name="links" />
 			</div>
 		</nav>
+		{#if showBack}
+			<div class="hidden md:block absolute top-0 left-0">
+				<div class="cursor-pointer flex items-center text-xl m-8 md:m-16" on:click={goBack}>
+					<span class="pb-1 pr-2">←</span>
+					<p>back</p>
+				</div>
+			</div>
+		{/if}
 		<div class="flex justify-center md:justify-start md:pl-16 items-center h-full">
 			<div class="text-center md:text-left">
 				<h1 class="text-5xl md:text-6xl text-primary mb-4">
